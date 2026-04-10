9491a8566861ff77f01a08e5c1580acd3b0e3592
Sud-San <sitaramsantra07+githhub@gmail.com>
2026-04-10 20:02:48 +0530

    header tag page

--- headtag.php
+++ headtag.php
@@ -1,21 +1,21 @@
-		<meta charset="UTF-8" />
-        <title><?php echo $company_name; ?></title>
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta content="Dream Class" name="description" />
-        <meta content="Business, Corporate, Startups, IT, Multipurpose, Responsive, HTML, Tailwind Css" name="keywords" />
-        <meta name="version" content="1.0.0" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta charset="UTF-8" />
+<title><?php echo $company_name; ?></title>
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+<meta content="Dream Class" name="description" />
+<meta content="Business, Corporate, Startups, IT, Multipurpose, Responsive, HTML, Tailwind Css" name="keywords" />
+<meta name="version" content="1.0.0" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
-        <!-- favicon -->
-		<link rel="icon" sizes="180x180" href="codez3.png" />
+<!-- favicon -->
+<link rel="icon" sizes="180x180" href="SkillRise_logo1.png" />
 
-        <!-- SweetAlert2 CSS -->
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
+<!-- SweetAlert2 CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
 
-        <!-- Css -->
-        <link href="assets/libs/tobii/css/tobii.min.css" rel="stylesheet">
-		<link href="assets/libs/tiny-slider/tiny-slider.css" rel="stylesheet">
-        <!-- Main Css -->
-        <link href="assets/libs/remixicon/fonts/remixicon.css" rel="stylesheet" type="text/css" />
-        <link rel="stylesheet" href="assets/css/tailwind.min.css" />
-        <link rel="stylesheet" href="assets/css/site-enhancements.css" />
+<!-- Css -->
+<link href="assets/libs/tobii/css/tobii.min.css" rel="stylesheet">
+<link href="assets/libs/tiny-slider/tiny-slider.css" rel="stylesheet">
+<!-- Main Css -->
+<link href="assets/libs/remixicon/fonts/remixicon.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" href="assets/css/tailwind.min.css" />
+<link rel="stylesheet" href="assets/css/site-enhancements.css" />
