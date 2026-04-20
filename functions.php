1000100d6b2490a8f639715a4e04e10057eb2b15
Sud-San <sitaramsantra07+githhub@gmail.com>
2026-04-20 15:21:55 +0530

    Functions file

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 // functions.php
-require_once 'connection.php';
+include 'connection.php';
 require __DIR__ . '/vendor/autoload.php'; // composer autoload for PHPMailer
 
 use PHPMailer\PHPMailer\PHPMailer;
@@ -9,36 +9,38 @@ use PHPMailer\PHPMailer\Exception;
 /**
  * send_email - send HTML/Plain email using PHPMailer (Gmail SMTP)
  */
-function send_email($to, $subject, $bodyHtml, $bodyPlain = '') {
+function send_email($to, $subject, $bodyHtml, $bodyPlain = '')
+{
     // *** REPLACE with your Gmail & App Password ***
     $smtpHost = 'smtp.gmail.com';
-    $smtpUser = 'codezy03@gmail.com';               // <-- put your gmail here
-    $smtpPass = 'utcd obzc begu popi';     // <-- put your app password here
-    $smtpPort = 587;
-    $smtpSecure = 'tls';
+    $smtpUser = 'codezy03@gmail.com'; 
+    $smtpPass = 'bjzmtwtfnadwxqbt'; // Consistent password from verify/forgot
+    $smtpPort = 465;
+    $smtpSecure = 'ssl';
 
     $mail = new PHPMailer(true);
     try {
+        // SMTP settings
         $mail->isSMTP();
-        $mail->Host       = $smtpHost;
-        $mail->SMTPAuth   = true;
-        $mail->Username   = $smtpUser;
-        $mail->Password   = $smtpPass;
+        $mail->Host = $smtpHost;
+        $mail->SMTPAuth = true;
+        $mail->Username = $smtpUser;
+        $mail->Password = $smtpPass;
         $mail->SMTPSecure = $smtpSecure;
-        $mail->Port       = $smtpPort;
+        $mail->Port = $smtpPort;
 
-        $mail->setFrom($smtpUser, 'Codezy Support');
+        $mail->setFrom($smtpUser, 'SkillRise Support');
         $mail->addAddress($to);
 
         $mail->isHTML(true);
         $mail->Subject = $subject;
-        $mail->Body    = $bodyHtml;
+        $mail->Body = $bodyHtml;
         $mail->AltBody = $bodyPlain ?: strip_tags($bodyHtml);
 
         $mail->send();
         return true;
     } catch (Exception $e) {
-        error_log('Mail error: '.$mail->ErrorInfo);
+        error_log('Mail error: ' . $mail->ErrorInfo);
         return false;
     }
 }
@@ -46,11 +48,13 @@ function send_email($to, $subject, $bodyHtml, $bodyPlain = '') {
 /**
  * get_user_by_email - returns assoc array or null
  */
-function get_user_by_email($email) {
+function get_user_by_email($email)
+{
     global $conn;
     $emailEsc = mysqli_real_escape_string($conn, $email);
-    $sql = "SELECT * FROM users_tbl WHERE user_email = '$emailEsc' LIMIT 1";
+    $sql = "SELECT * FROM user_tbl WHERE user_email = '$emailEsc' LIMIT 1";
     $res = mysqli_query($conn, $sql);
-    if ($res && mysqli_num_rows($res) > 0) return mysqli_fetch_assoc($res);
+    if ($res && mysqli_num_rows($res) > 0)
+        return mysqli_fetch_assoc($res);
     return null;
 }
