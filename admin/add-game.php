fc8573094e3ec58edf6fe25a801c3000bfcd8382
Sud-San <sitaramsantra07+githhub@gmail.com>
2026-04-24 20:03:59 +0530

    add game page

--- admin/add-game.php
+++ admin/add-game.php
@@ -148,7 +148,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <title><?php echo $is_edit ? 'Edit' : 'Add'; ?> Game | <?php echo $company_name; ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- App favicon -->
-    <link rel="shortcut icon" href="assets/images/favicon.ico">
+    <link rel="shortcut icon" href="../SkillRise_logo1.png">
     <!-- Theme Config Js -->
     <script src="assets/js/config.js"></script>
     <!-- Vendor css -->
