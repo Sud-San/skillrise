81875f6958372cdc3ada6d950a30840ea74f97aa
Sud-San <sitaramsantra07+githhub@gmail.com>
2026-04-24 20:14:02 +0530

    manage notification page

--- admin/notification.php
+++ admin/notification.php
@@ -52,7 +52,7 @@ $result = mysqli_query($conn, $display_query);
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <!-- App favicon -->
-    <link rel="shortcut icon" href="assets/images/favicon.ico">
+    <link rel="shortcut icon" href="../SkillRise_logo1.png">
 
     <!-- Theme Config Js -->
     <script src="assets/js/config.js"></script>
